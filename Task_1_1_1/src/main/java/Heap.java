5a97ddca5280d51142a1012b0fa1cc141faa8c37
Natalia Reshetnikova <nataliareshetnikova943@gmail.com>
2024-09-07 13:54:08 +0700

    add javadocs

--- Task_1_1_1/src/main/java/Heap.java
+++ Task_1_1_1/src/main/java/Heap.java
@@ -1,27 +1,49 @@
+/**
+ * Class for manage max-heap
+ */
 public class Heap {
+    /**
+     * Private field {@code heap}
+     */
     int[] heap;
 
+    /**
+     *  Constructor of new object
+     * @param array array what be heapified
+     */
     public Heap(int[] array) {
         heap = array;
         heapify();
     }
 
+    /**
+     * Converts array {@link Heap#heap} to max-heap
+     */
     private void heapify() {
         int len = heap.length;
         int start = len / 2;
         for (int i = start; i >= 0; i--) {
-            shiftDown(len, i);
+            siftDown(len, i);
         }
     }
 
+    /**
+     * Swaps two elements in array {@link Heap#heap}
+     * @param i first index
+     * @param j second index
+     */
     public void swap(int i, int j) {
         int temp = heap[i];
         heap[i] = heap[j];
         heap[j] = temp;
     }
 
-
-    public void shiftDown(int len, int index) {
+    /**
+     * Sifts down element on position {@code index}
+     * @param len current len of heap
+     * @param index index of element
+     */
+    public void siftDown(int len, int index) {
         int left = 2 * index + 1;
         int right = 2 * index + 2;
         int largest = index;
@@ -33,7 +55,7 @@ public class Heap {
         }
         if (largest != index) {
             swap(index, largest);
-            shiftDown(len, largest);
+            siftDown(len, largest);
         }
     }
 }
--- Task_1_1_1/src/main/java/Heapsort.java
+++ Task_1_1_1/src/main/java/Heapsort.java
@@ -1,11 +1,17 @@
+/**
+ * Class for heapsort
+ */
 public class Heapsort {
-
+    /**
+     * Sorts array of integers using heapsort
+     * @param array array of integers
+    */
     static void heapsort(int[] array) {
         Heap heap = new Heap(array);
         int len = array.length;
         for (int i = len - 1; i > 0; i--) {
             heap.swap(0, i);
-            heap.shiftDown(i, 0);
+            heap.siftDown(i, 0);
         }
     }
 }
