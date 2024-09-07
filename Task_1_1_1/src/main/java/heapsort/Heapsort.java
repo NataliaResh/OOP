2bd57658233dda54cc378c0b73c19ffcd420ead5
Natalia Reshetnikova <nataliareshetnikova943@gmail.com>
2024-09-08 04:26:40 +0700

    add whitespace

--- Task_1_1_1/src/main/java/heapsort/Heapsort.java
+++ Task_1_1_1/src/main/java/heapsort/Heapsort.java
@@ -13,7 +13,7 @@ public class Heapsort {
         Heap.heapify(array);
         int len = array.length;
         for (int i = len - 1; i > 0; i--) {
-            Heap.swap(array,0, i);
+            Heap.swap(array, 0, i);
             Heap.siftDown(array, i, 0);
         }
     }
