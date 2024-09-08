fcb760c006a4591b870f7eb547b4384980f5c6fd
NataliaResh <NataliaResh@users.noreply.github.com>
2024-09-08 04:22:22 +0000

    Deploying to gh-pages from @ NataliaResh/OOP@b510f1f0cb1b4bddd99cd01da392eef71c739b8b 🚀

--- Task_1_1_1/type-search-index.js
+++ Task_1_1_1/type-search-index.js
@@ -1,1 +1,1 @@
-typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"<Unnamed>","l":"Heap"},{"p":"<Unnamed>","l":"Heapsort"}]
+typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"heapsort","l":"Heap"},{"p":"heapsort","l":"Heapsort"}]
