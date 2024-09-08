8d3f7d8890b2bb0f7f63d99cbcc8aa4a71f5b8d1
Natalia Reshetnikova <nataliareshetnikova943@gmail.com>
2024-09-08 11:51:02 +0700

    add javadocs

--- Task_1_1_1/src/main/java/heapsort/Main.java
+++ Task_1_1_1/src/main/java/heapsort/Main.java
@@ -2,7 +2,14 @@ package heapsort;
 
 import java.util.Arrays;
 
+/**
+ * Main class.
+ */
 public class Main {
+    /**
+     * Main function (start of program).
+     * @param args arguments of command line.
+     */
     public static void main(String[] args) {
         int[] array = {2, 3, 1};
         Heapsort.heapsort(array);
