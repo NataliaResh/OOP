414ba41e0fe9e7434aec2434d9a012837581f0f5
Reshetnikova Natalia <nataliareshetnikova943@gmail.com>
2024-09-08 18:39:28 +0700

    fix make script

--- Task_1_1_1/make.sh
+++ Task_1_1_1/make.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-javac -d ./build/classes/main/java/heapsort ./src/main/java/heapsort/*.java
+javac -d ./build/classes/main/java ./src/main/java/heapsort/*.java
 javadoc -d ./docs -sourcepath ./src/main/java -subpackages heapsort
-java ./build/classes/main/java/heapsort/Main
+java -classpath ./build/classes/main/java/ heapsort.Main
