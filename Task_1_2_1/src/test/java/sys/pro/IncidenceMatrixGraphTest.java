2fc343b3d2697b506cbdf177b58d1e088f4dbf49
Natalia Reshetnikova <nataliareshetnikova943@gmail.com>
2024-09-23 17:52:36 +0700

    add test building graph

--- Task_1_2_1/src/test/java/sys/pro/IncidenceMatrixGraphTest.java
+++ Task_1_2_1/src/test/java/sys/pro/IncidenceMatrixGraphTest.java
@@ -29,4 +29,19 @@ public class IncidenceMatrixGraphTest {
         String ans = "1: \n2: 1\n3: 2, 6\n4: 1\n5: 4\n6: \n";
         Assert.assertEquals(result, ans);
     }
+
+    @Test
+    public void buildTest() {
+        IncidenceMatrixGraph matrix = new IncidenceMatrixGraph();
+        matrix.addNode(4);
+        matrix.addNode(2);
+        matrix.addEdge(2, 1);
+        matrix.addEdge(4, 1);
+        matrix.addEdge(5, 4);
+        matrix.addEdge(3, 2);
+        matrix.addNode(6);
+        matrix.addEdge(3, 6);
+        IncidenceMatrixGraph result = new IncidenceMatrixGraph("build/resources/test/tests/1.in");
+        Assert.assertTrue(matrix.isEqual(result));
+    }
 }
