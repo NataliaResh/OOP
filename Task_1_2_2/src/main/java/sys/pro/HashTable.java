a18601ebb4296a1b1922a2d7c9ec943096b4fbc2
Natalia Reshetnikova <nataliareshetnikova943@gmail.com>
2024-09-24 11:13:23 +0700

    add class HashTable

---
+++ Task_1_2_2/src/main/java/sys/pro/HashTable.java
@@ -0,0 +1,41 @@
+package sys.pro;
+
+import java.util.HashMap;
+
+public class HashTable<K, V> {
+    public void put(K key, V value) {
+
+    }
+
+    public V get(K key) {
+        return null;
+    }
+
+    public boolean containsKey(K key) {
+        return true;
+    }
+
+    public void updateValue(K key) {
+
+    }
+
+    @Override
+    public String toString() {
+        return "";
+    }
+
+    public void remove(K key) {
+    }
+
+    public V forEach() {
+        return null;
+    }
+
+    public boolean isEqual(HashTable<K, V> map) {
+        return true;
+    }
+
+    HashTable(K key, V value) {
+
+    }
+}
---
+++ Task_1_2_2/src/test/java/sys/pro/HashTableTest.java
@@ -0,0 +1,4 @@
+package sys.pro;
+
+public class HashTableTest {
+}
